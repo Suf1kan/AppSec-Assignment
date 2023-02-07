43439daec68ee3b4ec29a50166ba4ef2ab396ea7
Suf1kan <you@example.com>
2023-02-07 17:51:46 +0800

    Add project files.

---
+++ AppSec Assignment/Models/Member.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppSec_Assignment.Models
+{
+    public class Member
+    {
+
+    }
+}
